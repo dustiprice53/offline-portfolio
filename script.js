88ed1fb689a6444e83b2b3dff32caec32c2bf901
dustiprice53 <dustiprice53@gmail.com>
2026-04-14 18:30:26 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+unction showSection(sectionId) {
+
+  let sections = document.querySelectorAll('.section');
+
+
+
+  sections.forEach(section => {
+
+    section.classList.remove('active');
+
+  });
+
+
+
+  document.getElementById(sectionId).classList.add('active');
+
+}
